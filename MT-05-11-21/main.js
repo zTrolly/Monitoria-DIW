3c920ec819fc5d60bca0cedf41836c8e8dd5f70c
Breno Lopes do Carmo <61760048+zTrolly@users.noreply.github.com>
2021-11-05 16:12:40 -0300

    add comments

--- MT-05-11-21/main.js
+++ MT-05-11-21/main.js
@@ -1,17 +1,32 @@
 
+
+//pegamos o botão do darkmode
 let btnDarkMode = document.getElementById("btn_mode_dark");
+
+//pegamos o lampada vazada
 let lampadaVazia = document.getElementById("lampada-vazia");
+
+//pegamos a lampada preenchida
 let lampadaCheia = document.getElementById("lampada-cheia");
-let teste = document.getElementById("teste");
+
+//pegamos o html
 let html = document.querySelector('html');
 
+//pegamos o titulo
+let teste = document.getElementById("teste");
 
 
 
+//adicionamos um evento de click na div do botao
 btnDarkMode.addEventListener('click', () => {
+  //assim que a div for clicada adicionamos a classe 'active' no html
   html.classList.toggle('active');
+
+  // colocamos toggle tanto na lampada vazia quanto na preenchida
   lampadaCheia.classList.toggle('disabled');
   lampadaVazia.classList.toggle('disabled');
+
+  //vertificamos se o html tem a class active, se tiver trocamos o conteúdo do titulo
   if(html.classList.contains('active') == false ) {
     teste.innerHTML ="Ligth Mode On";
   }else{
