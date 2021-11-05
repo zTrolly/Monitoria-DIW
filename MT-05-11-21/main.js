867e5e657c83e76d80b27f315b2666ce04871bae
Breno Lopes do Carmo <61760048+zTrolly@users.noreply.github.com>
2021-11-05 14:48:26 -0300

    UPDATE

--- MT-05-11-21/main.js
+++ MT-05-11-21/main.js
@@ -2,14 +2,25 @@
 let btnDarkMode = document.getElementById("btn_mode_dark");
 let lampadaVazia = document.getElementById("lampada-vazia");
 let lampadaCheia = document.getElementById("lampada-cheia");
+let teste = document.getElementById("teste");
 let html = document.querySelector('html');
+
+
+
 
 btnDarkMode.addEventListener('click', () => {
   html.classList.toggle('active');
   lampadaCheia.classList.toggle('disabled');
   lampadaVazia.classList.toggle('disabled');
-
+  if(html.classList.contains('active') == false ) {
+    teste.innerHTML ="Ligth Mode On";
+  }else{
+    teste.innerHTML = "Dark Mode On"
+  }
 })
+
+
+
 
 
 
